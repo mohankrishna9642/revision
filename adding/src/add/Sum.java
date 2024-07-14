f6fa2c91a15a03aaf8cc6a294ea4d67b56d8b648
HP <HP@DESKTOP-N0OC3AN>
2024-07-14 18:07:54 +0530

    change the comment

--- adding/src/add/Sum.java
+++ adding/src/add/Sum.java
@@ -3,7 +3,7 @@ package add;
 public class Sum {
 	public  void addingg(Integer a,Integer b)
 	{
-		System.out.println("the sum is "+a+b);
+		System.out.println("the sum of "+a+b);
 	}
 	public static void main(String[] args) {
 		Sum nm=new Sum();
