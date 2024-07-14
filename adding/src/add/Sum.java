c5da96ce0250555fa989f0999b0b492fa8efb11a
HP <HP@DESKTOP-N0OC3AN>
2024-07-14 18:00:31 +0530

    commet latest code

--- adding/src/add/Sum.java
+++ adding/src/add/Sum.java
@@ -1,5 +1,13 @@
 package add;
 
 public class Sum {
+	public  void addingg(Integer a,Integer b)
+	{
+		System.out.println("the sum is "+a+b);
+	}
+	public static void main(String[] args) {
+		Sum nm=new Sum();
+		nm.addingg(10, 12);
+	}
 
 }
